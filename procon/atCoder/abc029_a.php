07a0b8ffcfa074629700713ece0e375928743fd8
takepan <takepan@gmail.com>
2016-02-14 23:54:55 +0900

    atCoder added.

---
+++ procon/atCoder/abc029_a.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+        Problem URL : http://abc029.contest.atcoder.jp/tasks/abc029_a
+
+        Result : AC
+        Time   : 56 ms
+        Memory : 4500 KB
+     */
+
+// Here your code !
+    $fp = fopen("php://stdin", "r+");
+    $s = trim(fgets($fp));
+    
+    echo $s . "s\n";
+    
