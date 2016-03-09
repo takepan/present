33aa61ab9379af685aa65ea0776e92fc8bf73e94
takepan <takepan@gmail.com>
2016-03-09 14:31:05 +0900

    add atCoder

---
+++ procon/atCoder/abc020_a.php
@@ -0,0 +1,18 @@
+<?php
+    /*
+        Problem URL : https://abc020.contest.atcoder.jp/tasks/abc020_a
+
+        Score  : 100
+        Result : AC
+        Time   : 55 ms
+        Memory : 4544 KB
+     */
+
+    $fp = fopen("php://stdin", "r+");
+    $N = trim(fgets($fp));
+    if ($N == '1') {
+        echo "ABC" . PHP_EOL;
+    }
+    if ($N == '2') {
+        echo "chokudai" . PHP_EOL;
+    }
