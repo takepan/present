81cd1b9a627f146b4f788a5fc11c074375a4582d
takepan <takepan@gmail.com>
2016-05-16 12:50:37 +0900

    use Closure

--- procon/atCoder/arc009_2.php
+++ procon/atCoder/arc009_2.php
@@ -4,13 +4,12 @@
 
     Score  : 100
     Result : AC
-    Time   : 93 ms
-    Memory : 5372 KB
+    Time   : 73 ms
+    Memory : 4664 KB
  */
 
 $arrR = explode(" ", trim(fgets(STDIN)));
 $arrR = array_flip($arrR);
-// var_dump($arrR);
 
 $arrAnswer = array();
 fscanf(STDIN, "%d", $N);
@@ -26,15 +25,9 @@ for ($i = 0; $i < $N; $i++) {
     $arrAnswer[] = array("takahashi" => (string)$a, "ours" => $tmp);
 }
 
-// var_dump($arrAnswer);
-
-foreach ((array) $arrAnswer as $key => $value) {
-    $sort[$key] = $value['ours'];
-}
-
-array_multisort($sort, SORT_ASC, $arrAnswer);
-
-// var_dump($arrAnswer);
+usort($arrAnswer, function ($a, $b) {
+    return $a['ours'] - $b['ours'];
+});
 
 foreach ($arrAnswer as $k => $v) {
     echo $v['takahashi'] . PHP_EOL;
