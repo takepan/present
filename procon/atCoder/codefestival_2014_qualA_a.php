b701d1eacd40da1972d04e8fa40982a1cd33b0ff
takepan <takepan@gmail.com>
2016-06-14 12:09:45 +0900

    add AtCoder

---
+++ procon/atCoder/codefestival_2014_qualA_a.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Problem URL : http://code-festival-2014-quala.contest.atcoder.jp/tasks/code_festival_qualA_a
+
+    Score  : 100
+    Result : AC
+    Time   : 60 ms
+    Memory : 4080 KB
+ */
+
+fscanf(STDIN, "%s", $S);
+echo $S . "2014" . PHP_EOL;
