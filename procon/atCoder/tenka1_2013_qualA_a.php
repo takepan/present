96d7d71e498e9d0c098b8535c582bc662356a67e
takepan <takepan@gmail.com>
2016-07-29 12:34:04 +0900

    add atCoder

---
+++ procon/atCoder/tenka1_2013_qualA_a.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+        Problem URL : http://tenka1-2013-quala.contest.atcoder.jp/tasks/tenka1_2013_qualA_a
+
+        Score  :
+        Result :
+        Time   : ms
+        Memory : KB
+     */
+
+    $s = 42;
+
+    while($s < 130000000) {
+        $s *= 2;
+    }
+
+    echo $s . PHP_EOL;
---
+++ procon/atCoder/tenka1_2015_qualA_a.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        Problem URL : http://tenka1-2015-quala.contest.atcoder.jp/tasks/tenka1_2015_qualA_a
+
+        Score  :
+        Result :
+        Time   : ms
+        Memory : KB
+     */
+
+    echo "654231\n";
+
