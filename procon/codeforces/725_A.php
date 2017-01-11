c54c49656681bd0307b82e657c6fbb569a03c992
takepan <takepan@gmail.com>
2017-01-12 05:54:16 +0900

    Revert "codeForces725A"
    This reverts commit 18789df9c5c543cce92cf8a8b9c211ebd6cf5775.

--- procon/codeforces/725_A.php
+++ procon/codeforces/725_A.php
@@ -1,29 +1,8 @@
 <?php
-    fscanf(STDIN, "%d", $N);
-    fscanf(STDIN, "%s", $S);
+	fscanf(STDIN, "%d", $N);
+	fscanf(STDIN, "%s", $S);
 
-    $intSL = strpos($S, ">");
-    $intSR = strpos($S, "<");
-    $intEL = strrpos($S, "<");
-    $intER = strrpos($S, ">");
+	$l = substr_count($S, '<');
+	$r = substr_count($S, '>');
 
-    // var_dump($intSL);
-    // var_dump($intSR);
-    // var_dump($intEL);
-    // var_dump($intER);
-
-    $ans = 0;
-    if ($intSL === false || $intSR === false || $intEL === false || $intER === false) {
-        $ans = $N;
-    } else {
-        if ($intSR < $intSL) {
-            $ans += $intSL;
-        }
-
-        if ($intER > $intEL) {
-            $ans += $N - $intEL - 1;
-        }
-    }
-
-    echo $ans;
-
+	echo abs($l - $r);
