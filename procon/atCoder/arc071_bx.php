d05cbb736862a8d14ef1bde57aa102f6bd94d570
Shinji Takeuchi <shinji.takeuchi.ww@ins.orix.jp>
2017-04-14 19:33:37 +0900

    追加

--- procon/atCoder/arc071_bx.php
+++ procon/atCoder/arc071_bx.php
@@ -28,16 +28,14 @@ for ($i = 0; $i < $m - 1; $i++) {
     $by[$i] = ($i + 1) * ($m - $i - 1);
 }
 for ($x = 0; $x < $n - 1; $x++) {
-    $dx = $xx[$x+1] - $xx[$x];
-    $total_x += $bx[$x] * $dx;
-    $total_x %= MOD;
+    for ($y = 0; $y < $m - 1; $y++) {
+        $dx = $xx[$x+1] - $xx[$x];
+        $dy = $yy[$y+1] - $yy[$y];
+        $tmp = $bx[$x] * $by[$y] * $dx * $dy;
+        $ans += ($tmp % MOD);
+        if ($ans > MOD) {
+            $ans %= MOD;
+        }
+    }
 }
-for ($y = 0; $y < $m - 1; $y++) {
-    $dy = $yy[$y+1] - $yy[$y];
-    $total_y += $by[$y] * $dy;
-    $total_y %= MOD;
-}
-
-$ans = $total_x * $total_y % MOD;
-
 echo $ans . PHP_EOL;
