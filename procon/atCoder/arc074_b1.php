d1ce1880ac7d25b40233fc0a60dfa209f431147d
Shinji Takeuchi <takepan@gmail.com>
2019-08-07 10:18:33 +0900

    modify arc074b

--- procon/atCoder/arc074_b1.php
+++ procon/atCoder/arc074_b1.php
@@ -2,37 +2,102 @@
     fscanf(STDIN, "%d", $N);
     $a = explode(" ", trim(fgets(STDIN)));
     $b = $a;
+
     $partA = array_splice($a, 0, $N * 2);
-    $partB = array_splice($b, $N, $N * 2);
+    $originalA = $partA;
     arsort($partA);
     $rawA = $partA;
     rsort($rawA);
     $backupA = $rawA;
     $sumA = array_sum(array_splice($rawA, 0, $N));
-    echo $sumA . PHP_EOL;
+    //echo $sumA . PHP_EOL;
     $sortA = array_flip(array_keys($partA));
     //var_dump($partA);
     //exit;
+    /*
     echo "sort: ";
     foreach ($sortA as $k => $v) {
         echo "{$k}->{$v} ";
     }
     echo PHP_EOL;
-    
+    */
+
     $line = $N - 1;
     $popA = 0;
     
-    $arr = [$sumA];
+    $arrA = [$sumA];
     for ($i = $N * 2 - 1; $i >= $N; $i--) {
-        printf("%d %d \n", $sortA[$i], $line);
+        //printf("%d %d \n", $sortA[$i], $line);
         if ($sortA[$i] <= $line) {
-            $sumA -= $backupA[$popA];
+            $sumA -= $originalA[$i];
             $popA++;
+
             $line++;
+            while (is_null($backupA[$line])) {
+                $line++;
+            }
             $sumA += $backupA[$line];
+            //printf("+ %d - %d # %d\n", $backupA[$line], $backupA[$popA]);
         }
-        $arr[] = $sumA;
+        $backupA[$sortA[$i]] = null;
+        $arrA[] = $sumA;
     }
     
-    var_dump($backupA);
-    var_dump($arr);
+    //var_dump($backupA);
+    //var_dump($partA);
+    //var_dump($arrA);
+
+
+    $partB = array_splice($b, $N, $N * 2);
+    $originalB = $partB;
+    asort($partB);
+    $rawB = $partB;
+    sort($rawB);
+    $backupB = $rawB;
+    $sumB = array_sum(array_splice($rawB, 0, $N));
+    //echo $sumB . PHP_EOL;
+    $sortB = array_flip(array_keys($partB));
+    //var_dump($partB);
+    //exit;
+    /*
+    echo "sort: ";
+    foreach ($sortB as $k => $v) {
+        echo "{$k}->{$v} ";
+    }
+    echo PHP_EOL;
+    */
+
+    $line = $N - 1;
+    $popB = 0;
+    
+    $arrB = [$sumB];
+    for ($i = 0; $i < $N; $i++) {
+        //printf("%d %d \n", $sortB[$i], $line);
+        if ($sortB[$i] <= $line) {
+            $sumB -= $originalB[$i];
+            //var_dump($backupB);
+            $popB++;
+
+            $line++;
+            while (is_null($backupB[$line])) {
+                $line++;
+            }
+            $sumB += $backupB[$line];
+            //printf("+ %d - %d\n", $backupB[$line], $originalB[$i]);
+        } else {
+            //echo "SKIP \n";
+        }
+        $backupB[$sortB[$i]] = null;
+        $arrB[] = $sumB;
+    }
+    
+    //var_dump($backupA);
+    //var_dump($partB);
+    //var_dump($arrA);
+    //var_dump($arrB);
+
+    $ans = -PHP_INT_MAX;
+    foreach ($arrA as $key => $val) {
+        $ans = max($ans, $arrA[$key] - $arrB[$N - $key]);
+    }
+    echo $ans;
